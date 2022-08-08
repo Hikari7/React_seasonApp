ad9175e2536a0018114725ce8a9a19a263d3860e
Hikari7 <h.kobe712@gmail.com>
2022-08-08 12:20:51 -0700

    changed component to class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div> Hi there!</div>;
-};
+//classベースのcomponent(GEO APIを呼び出している訳ではない)
+class App extends React.Component {
+  render() {
+    //remder method
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
+
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+
+//   return <div>Latitude: </div>;
+// };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
