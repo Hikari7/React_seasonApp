a0861af23a703b79fe7e1fda6f67fca364cff4e6
Hikari7 <h.kobe712@gmail.com>
2022-08-09 11:59:09 -0700

    added the if condition to judge it has an error or not

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ class App extends React.Component {
   //State must ne initialized when a component is created
   //だからconstrucrtorメソッドを作る
   constructor(props) {
-    super(props); //Reactの拡張機能を使うためのお決まり文句みたいなもん
+    super(props); //super: Reactの拡張機能(React.Component)を使うためのお決まり文句みたいなもん
 
     //THIS IS THE ONLY TIME we do direct assignment
     //to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
     //初期化したvalue/keyをstateにassignして初期化する
     //state objectを初期化する時、reasonableな値に初期化する必要があるのでnull
     //現在のところlatはnullだけど、getCurrentPositionを取得したら分かるようになる
@@ -19,18 +19,27 @@ class App extends React.Component {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         //we called setstate!(React componetを拡張した際にアプリコンポーネントに自動的に付与される機能)
-        //情報を更新したいときにはsetState functionを呼ぶことになる
+        //情報をupdateしたいときには必ず！setState functionを呼ぶことになる
+        //state objectへdirect assignmentすることはしない(this.state.latみたいに。必ずsetStateを使う)
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   //React says we have to define render!
   render() {
-    //remder method
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
 
-    return <div>Latitude: {this.state.lat}</div>;
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
