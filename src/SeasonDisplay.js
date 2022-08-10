98a20fb5d3004d72ada7d08655c5f1f12b26a2eb
Hikari7 <h.kobe712@gmail.com>
2022-08-10 12:20:40 -0700

    added get season func, passed state as props

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  //sesonDisplay用に仮の引数を渡しておく
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter"; //北半球にいるのであれば"summer"を返して、そうでなければ"winnter"を返す
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  // console.log(props.lat); seasonDisplayがちゃんとcurrent latが伝えられているか確認
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  const icon = season === "winter" ? "snowflake" : "sun";
+
+  return (
+    <div>
+      <i className={`${icon}`} />
+      <h1>{text}</h1>
+      <i className={`${icon}`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+// import "semantic-ui-css/semantic.min.css";
 
 //classベースのcomponent(GEO APIを呼び出している訳ではない)
 class App extends React.Component {
   //State must ne initialized when a component is created
+
+  //↓は、constructorのリファクタリング版
+  state = { lat: null, errorMessage: "" };
+
   //だからconstrucrtorメソッドを作る
-  constructor(props) {
-    super(props); //super: Reactの拡張機能(React.Component)を使うためのお決まり文句みたいなもん
+  // constructor(props) {
+  //   super(props); //super: Reactの拡張機能(React.Component)を使うためのお決まり文句みたいなもん
 
-    //THIS IS THE ONLY TIME we do direct assignment
-    //to this.state
-    this.state = { lat: null, errorMessage: "" };
-    //初期化したvalue/keyをstateにassignして初期化する
-    //state objectを初期化する時、reasonableな値に初期化する必要があるのでnull
-    //現在のところlatはnullだけど、getCurrentPositionを取得したら分かるようになる
+  //   //THIS IS THE ONLY TIME we do direct assignment
+  //   //to this.state
+  //   this.state = { lat: null, errorMessage: "" };
+  //   //初期化したvalue/keyをstateにassignして初期化する
+  //   //state objectを初期化する時、reasonableな値に初期化する必要があるのでnull
+  //   //現在のところlatはnullだけど、getCurrentPositionを取得したら分かるようになる
+  // }
 
+  componentDidMount() {
     //componentが画面に表示された瞬間(インスタンスを作成した瞬間にユーザーの現在地を取得できるようになる)
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //we called setstate!(React componetを拡張した際にアプリコンポーネントに自動的に付与される機能)
-        //情報をupdateしたいときには必ず！setState functionを呼ぶことになる
-        //state objectへdirect assignmentすることはしない(this.state.latみたいに。必ずsetStateを使う)
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+      //we called setstate!(React componetを拡張した際にアプリコンポーネントに自動的に付与される機能)
+      //情報をupdateしたいときには必ず！setState functionを呼ぶことになる
+      //state objectへdirect assignmentすることはしない(this.state.latみたいに。必ずsetStateを使う)
     );
   }
 
   //React says we have to define render!
   render() {
+    //1.緯度がどちらにあるかの確認
+    //2.それに伴って表示させる季節をここで書く
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
+      //took state from one component and pass it as a prop down to the child.
     }
 
     return <div>Loading!</div>;
